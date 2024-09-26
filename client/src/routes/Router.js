e6d4f7b8e73e880db8038753a8e2b3ddf901e871
OsuohaNnamdi <nnamdiosuohat@gmail.com>
2024-09-26 08:44:56 +0100

    the error fix

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -1,20 +1,21 @@
 import { lazy } from "react";
 import { Navigate } from "react-router-dom";
-import PastQuestionForm from "../pages/dashboard/studentDashboard/PastQuestionForm"; // Removed .jsx if not required
-import PastQuestions from "../pages/dashboard/studentDashboard/PastQuestions"; // Removed .jsx if not required
-import SearchPage from "../pages/StudentShare/SearchPage"; // Removed .jsx if not required
-import ChatPage from "../pages/StudentShare/ChatPage"; // Removed .jsx if not required
-import HomePage from "../pages/HomePage"; // Removed .jsx if not required
-import LoginPage from "../pages/auth/Login"; // Removed .jsx if not required
-import RegistrationPage from "../pages/auth/RegistrationPage"; // Removed .jsx if not required
-import StudentTable from "../pages/dashboard/studentDashboard/StudentTable"; // Removed .jsx if not required
-import CourseTable from "../pages/dashboard/studentDashboard/CourseTable"; // Removed .jsx if not required
-import AdminDashboard from '../pages/AdminDashboard'; // Removed .js if not required
 
-const FullLayout = lazy(() => import("../layouts/FullLayout/FullLayout"));
-const Dashboard = lazy(() => import("../pages/dashboard/studentDashboard/Dashboard"));
-const LecturerDashboard = lazy(() => import("../pages/dashboard/LecturerDashboard/LecturerDashboard"));
-const AddCourse = lazy(() => import("../pages/dashboard/LecturerDashboard/AddCourse"));
+// Lazy loading all pages
+const FullLayout = lazy(() => import("../layouts/FullLayout/FullLayout.js"));
+const PastQuestionForm = lazy(() => import("../pages/dashboard/studentDashboard/PastQuestionForm.jsx"));
+const PastQuestions = lazy(() => import("../pages/dashboard/studentDashboard/PastQuestions.jsx"));
+const SearchPage = lazy(() => import("../pages/StudentShare/SearchPage.jsx"));
+const ChatPage = lazy(() => import("../pages/StudentShare/ChatPage.jsx"));
+const HomePage = lazy(() => import("../pages/HomePage.jsx"));
+const LoginPage = lazy(() => import("../pages/auth/Login.jsx"));
+const RegistrationPage = lazy(() => import("../pages/auth/RegistrationPage.jsx"));
+const StudentTable = lazy(() => import("../pages/dashboard/studentDashboard/StudentTable.jsx"));
+const CourseTable = lazy(() => import("../pages/dashboard/studentDashboard/CourseTable.jsx"));
+const AdminDashboard = lazy(() => import("../pages/AdminDashboard.js"));
+const Dashboard = lazy(() => import("../pages/dashboard/studentDashboard/Dashboard.jsx"));
+const LecturerDashboard = lazy(() => import("../pages/dashboard/LecturerDashboard/LecturerDashboard.jsx"));
+const AddCourse = lazy(() => import("../pages/dashboard/LecturerDashboard/AddCourse.jsx"));
 
 const ThemeRoutes = [
   {
